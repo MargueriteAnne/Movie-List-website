9afb345ed0be80b1dd43d5e4a57222e6a0a47b08
MargueriteAnne <169045310+MargueriteAnne@users.noreply.github.com>
2024-05-12 22:06:06 +0200

    Update gallery.php

--- gallery.php
+++ gallery.php
@@ -13,6 +13,27 @@
             <input type="file" name="fileToUpload" id="fileToUpload">
             <input type="submit" value="Upload Image" name="submit">
         </form>
+        <div id="imagePreview"></div>
+    
+        <script>
+            // Ajax for image upload
+            document.getElementById("uploadForm").addEventListener("submit", function(event){
+                event.preventDefault(); // Prevent default form submission
+
+                var formData = new FormData(this);
+
+                // Send Ajax request
+                var xhr = new XMLHttpRequest();
+                xhr.open("POST", "upload.php", true);
+                xhr.onreadystatechange = function () {
+                    if (xhr.readyState == 4 && xhr.status == 200) {
+                        // Display uploaded image
+                        document.getElementById("imagePreview").innerHTML = xhr.responseText;
+                    }
+                };
+                xhr.send(formData);
+            });
+        </script>
     <?php else: ?>
         <p>You need to <a href="login.php">login</a> to upload images.</p>
     <?php endif; ?>

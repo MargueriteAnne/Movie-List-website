7d46f94513ea29b8176f2bd3bdb0b6486bcaf18c
MargueriteAnne <169045310+MargueriteAnne@users.noreply.github.com>
2024-05-12 11:56:43 +0200

    Create config.php

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'menu' => [
+        'home' => 'Home',
+        'Booking' => 'Booking',
+        'hotel' => 'Hotel',
+        'car rent' => 'Car Rent',
+        'contact' => 'Contact'
+    ],
+    'common_data' => [
+        'company_name' => 'Your Travel Agency',
+        'address' => 'steet, Kecskemét, Hungary',
+        'phone' => '+36-20-496-9530'
+    ]
+];
+?>

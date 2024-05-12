9321584781da2c85db2a15ba43eea259c01755e3
MargueriteAnne <169045310+MargueriteAnne@users.noreply.github.com>
2024-05-12 21:17:50 +0200

    Create contact.php

---
+++ contact.php
@@ -0,0 +1,6 @@
+<form action="submit_message.php" method="post">
+    Name: <input type="text" name="name" required><br>
+    Email: <input type="email" name="email" required><br>
+    Message: <textarea name="message" required></textarea><br>
+    <input type="submit" value="Send Message">
+</form>

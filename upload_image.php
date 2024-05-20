158af1c4a8960905f5c45156c985138d748a19ad
MargueriteAnne <169045310+MargueriteAnne@users.noreply.github.com>
2024-05-20 18:28:34 +0200

    Update upload_image.php

--- upload_image.php
+++ upload_image.php
@@ -37,25 +37,25 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     // Check if file already exists
     if (file_exists($target_file)) {
-        echo "Sorry, file already exists.";
+        echo '<p class="white-text">Sorry, file already exists.</p>';
         $uploadOk = 0;
     }
 
     // Check file size
-    if ($_FILES["fileToUpload"]["size"] > 500000) {
-        echo "Sorry, your file is too large.";
+    if ($_FILES["fileToUpload"]["size"] > 3145728) {
+        echo '<p class="white-text">Sorry, your file is too large.</p>';
         $uploadOk = 0;
     }
 
     // Allow certain file formats
     if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
-        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        echo '<p class="white-text">Sorry, only JPG, JPEG, PNG & GIF files are allowed.</p>';
         $uploadOk = 0;
     }
 
     // Check if $uploadOk is set to 0 by an error
     if ($uploadOk == 0) {
-        echo "Sorry, your file was not uploaded.";
+        echo '<p class="white-text">Sorry, your file was not uploaded.</p>';
     
     } else {
         if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
@@ -68,9 +68,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 die("Error: " . $sql . "<br>" . $conn->error);
             }
 
-            echo "The file ". htmlspecialchars(basename($_FILES["fileToUpload"]["name"])). " has been uploaded.";
+            echo '<p class="white-text"> The file ' . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])). ' has been uploaded.;</p>';
         } else {
-            echo "Sorry, there was an error uploading your file.";
+            echo '<p class="white-text">Sorry, there was an error uploading your file.</p>';
         }
     }
 }
@@ -80,6 +80,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <form action="index.php?menu=upload_image" method="post" enctype="multipart/form-data">
     Select image to upload:
     <input type="file" name="fileToUpload" id="fileToUpload" >
+    <br>
     <input type="submit" value="Upload Image" name="submit">
 </form>
 
